0bdce9edd0392a109ee222a5a56b46429809a0e7
Akeem Amusat <aoamusat@gmail.com>
2023-09-04 20:54:53 +0100

    Added seeder to installed apps

--- herconomy/settings.py
+++ herconomy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apimonitor.apps.ApimonitorConfig",
+    "seeder.apps.SeederConfig",
     "rest_framework",
 ]
 
